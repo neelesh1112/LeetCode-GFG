ad11bb6923821da07609efc2e1d6df87c71be66d
Neelesh Singh <89899935+neelesh1112@users.noreply.github.com>
2023-10-02 00:06:12 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Peak element/peak-element.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/* The function should return the index of any
+   peak element present in the array */
+
+// arr: input array
+// n: size of array
+class Solution
+{
+    public:
+    int peakElement(int a[], int n)
+    {
+       // Your code here
+        int ans=0;int data=a[0];
+        for(int i=1;i<n;i++)
+        {
+           if(a[i]>data)
+           {
+               data=a[i];
+               ans=i;
+           }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n], tmp[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+			tmp[i] = a[i];
+		}
+		bool f=0;
+		Solution ob;
+		
+		int A = ob. peakElement(tmp,n);
+		
+		if(A<0 and A>=n)
+		    cout<<0<<endl;
+		else
+		{
+    		if(n==1 and A==0)
+    		    f=1;
+    		else if(A==0 and a[0]>=a[1])
+    		    f=1;
+    		else if(A==n-1 and a[n-1]>=a[n-2])
+    		    f=1;
+    		else if(a[A]>=a[A+1] and a[A]>= a[A-1])
+    		    f=1;
+    		else
+    		    f=0;
+    		cout<<f<<endl;
+		}
+		
+	}
+
+	return 0;
+}
+// } Driver Code Ends
