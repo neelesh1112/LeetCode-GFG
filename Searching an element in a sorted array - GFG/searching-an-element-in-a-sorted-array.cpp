6f049e18d77fd9d61f438b3dbde5ee56e39183c4
Neelesh Singh <89899935+neelesh1112@users.noreply.github.com>
2023-10-17 22:58:48 +0530

    Added solution - LeetHub

---
+++ Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find element in sorted array
+    // arr: input array
+    // N: size of array
+    // K: element to be searche
+    int searchInSorted(int arr[], int N, int K) 
+    { 
+    
+       // Your code here
+
+        int beg = 0;
+        int end = N-1;
+     
+        while(beg<=end){
+             int mid = beg+ (end - beg)/2;  // Find middle element of array
+             if(arr[mid] == K){
+                return 1;
+             }
+    
+            //  Ignore left half
+             else if(arr[mid] < K){
+                beg  = mid+1;
+             }
+    
+            //  Ingore Right Half
+             else{
+                end = mid-1;
+             }
+        }
+
+    return -1;  
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(void) 
+{ 
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        cout << ob.searchInSorted(arr, n, k) << endl;
+
+    }
+
+	return 0; 
+} 
+
+// } Driver Code Ends
