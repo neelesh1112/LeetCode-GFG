3d4a91700cb661008df277794c859aff03b1aa79
Neelesh Singh <89899935+neelesh1112@users.noreply.github.com>
2023-10-24 23:24:34 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest Common Substring/longest-common-substring.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String input[] = read.readLine().trim().split(" ");
+            int n = Integer.parseInt(input[0]);
+            int m = Integer.parseInt(input[1]);
+            
+            String S1 = read.readLine().trim();
+            String S2 = read.readLine().trim();
+
+            Solution ob = new Solution();
+            System.out.println(ob.longestCommonSubstr(S1, S2, n, m));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    int longestCommonSubstr(String S1, String S2, int n, int m){
+        // code here
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<=n;j++){
+                String s=S1.substring(i,j);
+                if(S2.contains(s) && s.length()>ans) ans=s.length();
+            }
+        }
+        return ans;
+    }
+}
